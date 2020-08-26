94de5af2ce7ada2340546a442fbb9d20226e10ab
Marcin Stachniuk <mstachniuk@gmail.com>
2020-08-26 22:23:26 +0200

    Better cleaner solution for Q1 03 Urlify

--- Java/Ch 01. Arrays and Strings/Q1_03_URLify/Question.java
+++ Java/Ch 01. Arrays and Strings/Q1_03_URLify/Question.java
@@ -5,23 +5,14 @@ import CtCILibrary.AssortedMethods;
 public class Question {
 	// Assume string has sufficient free space at the end
 	public static void replaceSpaces(char[] str, int trueLength) {
-		int spaceCount = 0, index, i = 0;
-		for (i = 0; i < trueLength; i++) {
+		for (int i = trueLength; i >= 0; i--) {
 			if (str[i] == ' ') {
-				spaceCount++;
-			}
-		}
-		index = trueLength + spaceCount * 2;
-		if (trueLength < str.length) str[trueLength] = '\0';
-		for (i = trueLength - 1; i >= 0; i--) {
-			if (str[i] == ' ') {
-				str[index - 1] = '0';
-				str[index - 2] = '2';
-				str[index - 3] = '%';
-				index = index - 3;
-			} else {
-				str[index - 1] = str[i];
-				index--;
+				for (int j = str.length - 1; j >= i; j--) {
+					str[j] = str[j-2];
+				}
+				str[i] = '%';
+				str[i+1] = '2';
+				str[i+2] = '0';
 			}
 		}
 	}
